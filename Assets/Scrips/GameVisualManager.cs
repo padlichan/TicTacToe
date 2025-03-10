6385a27bb4120ddd547273f4bf095ecd6b9db8d9
Lili <lili.berenyi@gmail.com>
2025-03-10 16:51:46 +0000

    Synchronise object transform

--- Assets/Scrips/GameVisualManager.cs
+++ Assets/Scrips/GameVisualManager.cs
@@ -22,9 +22,8 @@ public class GameVisualManager : NetworkBehaviour
     private void SpawnObjectRpc(int x, int y)
     {
         Debug.Log("SpawnObject");
-        Transform spawnedCrossTransform = Instantiate(crossPrefab);
+        Transform spawnedCrossTransform = Instantiate(crossPrefab, GetGridWorldPosition(x, y), Quaternion.identity);
         spawnedCrossTransform.GetComponent<NetworkObject>().Spawn(true);
-        spawnedCrossTransform.position = GetGridWorldPosition(x, y);
     }
 
     private Vector2 GetGridWorldPosition(int x, int y)
